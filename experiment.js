ce616f42ffeb2829a481fa4f69b05707e499d0e1
Chris Rabl <rabl.christopher@gmail.com>
2016-01-20 23:50:38 -0700

    Quick experiment

--- experiment.js
+++ experiment.js
@@ -2,34 +2,61 @@
 
 let fs = require('fs');
 let xml2js = require('xml2js');
-let feed = require('feed-read');
+let feedReader = require('feed-read');
 let moment = require('moment');
+let shuffle = require('knuth-shuffle').knuthShuffle;
 
 let Q = require('Q');
 
 let feeds = [{
   url: 'http://www.fool.ca/feed',
+  obscurity: 0,
+  specificity: 0
 }, {
   url: 'http://www.greaterfool.ca/feed/',
+  obscurity: 0,
+  specificity: 0
 }, {
-  url: 'http://www.donrcampbell.com/rss'
+  url: 'http://www.donrcampbell.com/rss',
+  obscurity: 0,
+  specificity: 0
+}, {
+  url: 'http://blog.reincanada.com/rss.xml',
+  obscurity: 0,
+  specificity: 0
+}, {
+  url: 'http://edmontonrealestateblog.com/feed/',
+  obscurity: 0,
+  specificity: 0
+}, {
+  url: 'https://www.ratehub.ca/blog/home/feed/',
+  obscurity: 0,
+  specificity: 0
 }];
 
-let articles = feeds.map(url => {
+let allArticles = feeds.map(feed => {
   let deferred = Q.defer();
 
-  feed(url, (err, articles) => {
-    if (err) {
-      deferred.reject(err);
-    }
+  feedReader(feed.url, (err, articles) => {
+    if (err) { deferred.reject(err); }
 
-    let todayArticles = articles.filter(article => {
-      // return articles published in the last day
-      return moment().diff(moment(article.published), 'days') <= 1;
-    }).map(article => ({
-      title: article.title,
-      // content: article.content
-    }));
+    let dates = articles.map(article => moment(article.published));
+
+    let averagePublicationFrequency = dates
+      .map((current, index) => {
+        let last = index === 0 ? moment() : dates[index - 1];
+        return last.diff(current, 'days');
+      })
+      .reduce((total, n) => total + n, 0) / (dates.length);
+
+    // TODO: will have to add a nonce in here somewhere to keep the order sufficiently random
+    let todayArticles = articles
+      .filter(article => moment().diff(moment(article.published), 'days') <= 20)
+      .map(article => ({
+        title: article.title,
+        frequency: averagePublicationFrequency
+        // content: article.content
+      }));
 
     deferred.resolve(todayArticles);
   });
@@ -37,23 +64,9 @@ let articles = feeds.map(url => {
   return deferred.promise;
 });
 
-Q.all(articles).then(collection => {
-  let flattened = collection.reduce((items, current) => items.concat(current), []);
-  console.log(flattened);
-});
-
-// feed("http://craphound.com/?feed=rss2", (err, articles) => {
-//   if (err) throw err;
-//
-//   let todayArticles = articles.filter(article => moment().diff(moment(article.published), 'days') <= 1);
-//
-//   // Each article has the following properties:
-//   //
-//   //   * "title"     - The article title (String).
-//   //   * "author"    - The author's name (String).
-//   //   * "link"      - The original article link (String).
-//   //   * "content"   - The HTML content of the article (String).
-//   //   * "published" - The date that the article was published (Date).
-//   //   * "feed"      - {name, source, link}
-//   //
-// });
+Q.all(allArticles)
+ .then(collection => collection.reduce((flat, arr) => flat.concat(arr), []))
+ .then(articles => shuffle([...articles]))
+ .then(articles => {
+   console.log(JSON.stringify(articles, null, 2));
+ });
